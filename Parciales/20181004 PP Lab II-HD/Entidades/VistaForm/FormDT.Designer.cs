3559c174f675d08e593544a62e499aa774c04760
francofami <31368175+francofami@users.noreply.github.com>
2018-10-28 18:28:06 -0300

    Add files via upload

---
+++ Parciales/20181004 PP Lab II-HD/Entidades/VistaForm/FormDT.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace VistaForm
+{
+    partial class FormDT
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Crear = new System.Windows.Forms.Button();
+            this.Validar = new System.Windows.Forms.Button();
+            this.Apellido = new System.Windows.Forms.Label();
+            this.DNI = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.Edad = new System.Windows.Forms.Label();
+            this.Experiencia = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Crear
+            // 
+            this.Crear.Location = new System.Drawing.Point(69, 326);
+            this.Crear.Name = "Crear";
+            this.Crear.Size = new System.Drawing.Size(75, 36);
+            this.Crear.TabIndex = 0;
+            this.Crear.Text = "Crear";
+            this.Crear.UseVisualStyleBackColor = true;
+            this.Crear.Click += new System.EventHandler(this.Crear_Click);
+            // 
+            // Validar
+            // 
+            this.Validar.Location = new System.Drawing.Point(247, 326);
+            this.Validar.Name = "Validar";
+            this.Validar.Size = new System.Drawing.Size(75, 36);
+            this.Validar.TabIndex = 1;
+            this.Validar.Text = "Validar";
+            this.Validar.UseVisualStyleBackColor = true;
+            this.Validar.Click += new System.EventHandler(this.Validar_Click);
+            // 
+            // Apellido
+            // 
+            this.Apellido.AutoSize = true;
+            this.Apellido.Location = new System.Drawing.Point(66, 104);
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Size = new System.Drawing.Size(44, 13);
+            this.Apellido.TabIndex = 2;
+            this.Apellido.Text = "Apellido";
+            // 
+            // DNI
+            // 
+            this.DNI.AutoSize = true;
+            this.DNI.Location = new System.Drawing.Point(66, 203);
+            this.DNI.Name = "DNI";
+            this.DNI.Size = new System.Drawing.Size(26, 13);
+            this.DNI.TabIndex = 3;
+            this.DNI.Text = "DNI";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(66, 58);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(44, 13);
+            this.Nombre.TabIndex = 4;
+            this.Nombre.Text = "Nombre";
+            // 
+            // Edad
+            // 
+            this.Edad.AutoSize = true;
+            this.Edad.Location = new System.Drawing.Point(66, 153);
+            this.Edad.Name = "Edad";
+            this.Edad.Size = new System.Drawing.Size(32, 13);
+            this.Edad.TabIndex = 5;
+            this.Edad.Text = "Edad";
+            // 
+            // Experiencia
+            // 
+            this.Experiencia.AutoSize = true;
+            this.Experiencia.Location = new System.Drawing.Point(66, 247);
+            this.Experiencia.Name = "Experiencia";
+            this.Experiencia.Size = new System.Drawing.Size(62, 13);
+            this.Experiencia.TabIndex = 6;
+            this.Experiencia.Text = "Experiencia";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(202, 201);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(202, 245);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(202, 151);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // FormDT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(386, 411);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Experiencia);
+            this.Controls.Add(this.Edad);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.DNI);
+            this.Controls.Add(this.Apellido);
+            this.Controls.Add(this.Validar);
+            this.Controls.Add(this.Crear);
+            this.Name = "FormDT";
+            this.Text = "FormDT";
+            this.Load += new System.EventHandler(this.FormDT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Crear;
+        private System.Windows.Forms.Button Validar;
+        private System.Windows.Forms.Label Apellido;
+        private System.Windows.Forms.Label DNI;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Label Edad;
+        private System.Windows.Forms.Label Experiencia;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Parciales/20181004 PP Lab II-HD/Entidades/VistaForm/FormDT.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace VistaForm
+{
+    public partial class FormDT : Form
+    {
+        DirectorTecnico dt;
+
+        public FormDT()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDT_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Crear_Click(object sender, EventArgs e)
+        {
+            this.dt = new DirectorTecnico(this.textBox1.Text, this.textBox2.Text, (int)this.numericUpDown3.Value, (int)this.numericUpDown1.Value, (int)this.numericUpDown2.Value);
+
+            MessageBox.Show("Se ha creado el DT");
+            
+        }
+
+        private void Validar_Click(object sender, EventArgs e)
+        {
+            if(Object.Equals(dt,null))
+            {
+                MessageBox.Show("Aún no se ha creado el DT del formulario");
+            }
+            else
+            if(this.dt.ValidarAptitud()==true)
+            {
+                MessageBox.Show("El DT es apto");
+            }
+            else
+            {
+                MessageBox.Show("El DT no es apto");
+            }
+        }
+    }
+}
